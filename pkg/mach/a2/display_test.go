9a85581102ceecae6f9ef23196f0d04ccfb4e195
Peter Evans <peter@modul8r.com>
2018-10-20 18:45:10 -0500

    Add AuxSegment test

--- pkg/mach/a2/display_test.go
+++ pkg/mach/a2/display_test.go
@@ -1,6 +1,9 @@
 package a2
 
-import "github.com/stretchr/testify/assert"
+import (
+	"github.com/pevans/erc/pkg/mach"
+	"github.com/stretchr/testify/assert"
+)
 
 func (s *a2Suite) TestDisplayMode() {
 	s.comp.DisplayMode = 123
@@ -16,4 +19,25 @@ func (s *a2Suite) TestDisplaySetMode() {
 
 	displaySetMode(s.comp, 222)
 	assert.Equal(s.T(), 222, s.comp.DisplayMode)
+}
+
+func (s *a2Suite) TestDisplayAuxSegment() {
+	cases := []struct {
+		memMode int
+		addr    mach.DByte
+		want    *mach.Segment
+	}{
+		{0, 0x444, nil},
+		{Mem80Store, 0x444, nil},
+		{Mem80Store | MemHires, 0x444, s.comp.Aux},
+		{0, 0x2444, nil},
+		{Mem80Store, 0x2444, nil},
+		{Mem80Store | MemHires, 0x2444, nil},
+		{Mem80Store | MemHires | MemPage2, 0x2444, s.comp.Aux},
+	}
+
+	for _, c := range cases {
+		s.comp.MemMode = c.memMode
+		assert.Equal(s.T(), c.want, displayAuxSegment(s.comp, c.addr))
+	}
 }
