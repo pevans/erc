1ea2d0042b14e93b2faaefde1e87c59d15eedb07
Peter Evans <peter@modul8r.com>
2018-10-22 23:11:07 -0500

    Add consts, phase code

--- pkg/mach/a2/disk.go
+++ pkg/mach/a2/disk.go
@@ -7,10 +7,61 @@ import (
 )
 
 const (
+	// VolumeMarker is a hard-coded number that encoding software will
+	// expect to see in the track padding.
+	DDVolumeMarker = 0xFE
+
+	// NumTracks is the number of tracks that can be contained on a
+	// disk.
+	DDNumTracks = 35
+
+	// NumSectors is the number of sectors that each track contains.
+	DDNumSectors = 16
+
+	// LogSectorLen is the length of a logical sector, which is 256
+	// bytes.
+	LogSectorLen = 0x100
+
+	// LogTrackLen is the length of a logical track, consisting of 16
+	// logical sectors, which are each 256 bytes long. It thus holds 4
+	// kilobytes of data.
+	LogTrackLen = LogSectorLen * DDNumSectors
+
+	// PhysSectorLen is the length of a physical sector
+	PhysSectorLen = 0x1A0
+
+	// PhysTrackLen is the length of a physical track, consisting of 16
+	// physical sectors.
+	PhysTrackLen = PhysSectorLen * DDNumSectors
+
+	// A sector header consists of some byte markers--all byte markers
+	// in 6-and-2 encoding are 3 bytes long--and also some metadata,
+	// such as the track number, the sector number, the volume, and an
+	// XOR'd combination of all three.
+	PhysSectorHeader = 0x13
+
+	// The track header is 48 bytes of--well, nothing really, just
+	// padding.
+	PhysTrackHeader = 0x30
+)
+
+const (
 	// DDMaxSteps is the maximum number of steps we can move the drive
 	// head before running out of tracks on the disk. (Note that steps
 	// are half of the length of a track; 35 tracks, 70 steps.)
 	DDMaxSteps = 70
+
+	// DDMaxSectorPos is the highest sector position that we can allow
+	// within a given track. (0xFFF = 4k - 1.)
+	DDMaxSectorPos = 0xFFF
+
+	// DD140K is the number of bytes in 140 kilobytes.
+	DD140K = 143360
+
+	// DD140KNib is the capacity of the segment we will create for
+	// nibblized data, whether from 140k logical data or just any-old
+	// NIB file.
+	DD140KNib = 250000
 )
 
 const (
@@ -37,10 +88,6 @@ const (
 
 	// DDWrite indicates that we are in write mode for the drive.
 	DDWrite
-)
-
-const (
-	EncTrackLen = 0x1a00
 )
 
 // A DiskDrive represents the state of a virtual Disk II drive.
@@ -141,4 +188,43 @@ func (d *DiskDrive) Step(offset int) {
 
 	// The sector position also resets when the drive motor steps
 	d.SectorPos = 0
+}
+
+func DiskPhase(addr mach.DByte) int {
+	phase := -1
+
+	switch addr & 0xF {
+	case 0x1:
+		phase = 1
+	case 0x3:
+		phase = 2
+	case 0x5:
+		phase = 3
+	case 0x7:
+		phase = 4
+	}
+
+	return phase
+}
+
+//  0  1  2  3  4     phase transition
+var phaseTransitions = []int{
+	0, 0, 0, 0, 0, // no phases
+	0, 0, 1, 0, -1, // phase 1
+	0, -1, 0, 1, 0, // phase 2
+	0, 0, -1, 0, 1, // phase 3
+	0, 1, 0, -1, 0, // phase 4
+}
+
+func (d *DiskDrive) StepPhase(addr mach.DByte) {
+	phase := DiskPhase(addr)
+
+	if phase < 0 || phase > 4 {
+		return
+	}
+
+	offset := phaseTransitions[(d.Phase*5)+phase]
+	d.Step(offset)
+
+	d.Phase = phase
 }
