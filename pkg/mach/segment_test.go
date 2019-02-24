cbba3f82031e8dba87f8d822fb1e649c1270aff4
Peter Evans <peter@modul8r.com>
2019-02-24 17:40:48 -0600

    Add missing tests for segment operations

--- pkg/mach/segment_test.go
+++ pkg/mach/segment_test.go
@@ -41,3 +41,35 @@ func TestByteSlice(t *testing.T) {
 		assert.Equal(t, c.want, ByteSlice(c.in))
 	}
 }
+
+func TestSize(t *testing.T) {
+	s := NewSegment(100)
+	assert.Equal(t, 100, s.Size())
+}
+
+func TestCopySlice(t *testing.T) {
+	cases := []struct {
+		wantWritten int
+		wantError   bool
+		segmentSize int
+		start       int
+		byteSlice   []Byte
+	}{
+		{0, true, 1, -1, []Byte{}},
+		{0, true, 0, 1, ByteSlice([]byte{1, 2})},
+		{3, false, 5, 0, ByteSlice([]byte{1, 2, 3})},
+	}
+
+	for _, c := range cases {
+		s := NewSegment(c.segmentSize)
+
+		writ, err := s.CopySlice(c.start, c.byteSlice)
+		assert.Equal(t, c.wantWritten, writ)
+
+		if c.wantError {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+	}
+}
