9f6239ed681e9a6b1549f6da8f2fc7eda9a52815
Peter Evans <peter@modul8r.com>
2019-02-24 15:57:30 -0600

    Add tests for Int addressor, Plus function

--- pkg/mach/addressor_test.go
+++ pkg/mach/addressor_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIntAddr(t *testing.T) {
+	i := Int(123)
+
+	assert.Equal(t, 123, i.Addr())
+}
+
 func TestByteAddr(t *testing.T) {
 	b := Byte(123)
 
@@ -16,4 +22,11 @@ func TestDByteAddr(t *testing.T) {
 	db := DByte(123)
 
 	assert.Equal(t, 123, db.Addr())
+}
+
+func TestPlus(t *testing.T) {
+	a := Int(123)
+	b := Plus(a, 123)
+
+	assert.Equal(t, 246, b.Addr())
 }
