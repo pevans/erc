dc55bb7c02e42ab8c675b20b2b4a7ffcabe1081f
Peter Evans <peter@modul8r.com>
2019-06-16 13:55:27 -0500

    Add processor loop

---
+++ cmd/erc/process.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+func processorLoop() {
+	for {
+		if err := emulator.Processor.Process(); err != nil {
+			log.Error(errors.Wrap(err, "main loop received error from processor"))
+			return
+		}
+
+		time.Sleep(time.Microsecond)
+	}
+}
