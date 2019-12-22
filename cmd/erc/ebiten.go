9ddab154edc51320b9c052a00fc586b95d607249
Peter Evans <peter@modul8r.com>
2019-12-22 11:33:54 -0600

    If drawing is skipped, don't attempt to draw the image

--- cmd/erc/ebiten.go
+++ cmd/erc/ebiten.go
@@ -19,7 +19,16 @@ func gameLoop() error {
 // ebitenLoop is the "run loop" of our graphics logic, which we use both
 // to implement processor speed and frame rate.
 func ebitenLoop(screen *ebiten.Image) error {
+	// ebiten gives us a way to see if this result would even be
+	// considered, and if not, we can return immediately.
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	// The Draw method will give us what the current screen should look
+	// like.
 	updatedScreen := emulator.Drawer.Draw()
 
+	// Which is then flashed to ebiten.
 	return screen.DrawImage(updatedScreen, nil)
 }
