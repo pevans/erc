6d2c02f0f0f89b86eeb6ba214a8b2aab12d8130e
Peter Evans <peter@modul8r.com>
2021-02-11 00:19:07 -0600

    Add missing Fatal method, plus fix linting

--- pkg/boot/logger.go
+++ pkg/boot/logger.go
@@ -7,16 +7,25 @@ import (
 	"github.com/pkg/errors"
 )
 
+// A LogLevel is some level at which, if a message is at or exceeds its
+// threshold, we will log the message being passed to us.
 type LogLevel int
 
+// A Logger is a device that can log messages with respect to a given log level.
 type Logger struct {
 	logger *log.Logger
 	Level  LogLevel
 }
 
 const (
+	// LogNothing essentially is the level at which nothing will be logged out.
 	LogNothing LogLevel = iota
+
+	// LogError is the main error type you'd use when something is effectively
+	// "wrong" with execution.
 	LogError
+
+	// LogDebug is the error type you'd use for informational/development work.
 	LogDebug
 )
 
@@ -74,10 +83,18 @@ func (l *Logger) Errorf(fmt string, vals ...interface{}) {
 	}
 }
 
+// Error will log an error message (if allowed).
 func (l *Logger) Error(vals ...interface{}) {
 	if l.CanLog(LogError) {
 		l.logger.Println(vals...)
 	}
+}
+
+// Fatal will do what Error would do, with the added step of it'll exit the
+// program afterward.
+func (l *Logger) Fatal(vals ...interface{}) {
+	l.Error(vals...)
+	os.Exit(1)
 }
 
 // Debugf will log a debug message (if allowed).
@@ -87,6 +104,7 @@ func (l *Logger) Debugf(fmt string, vals ...interface{}) {
 	}
 }
 
+// Debug will log a debug message (if allowed).
 func (l *Logger) Debug(vals ...interface{}) {
 	if l.CanLog(LogDebug) {
 		l.logger.Println(vals...)
