b61373ebb4ffa32750e6a1d2c02e77a379b0ff9a
Peter Evans <peter@modul8r.com>
2023-10-10 00:30:23 -0500

    Print metrics on shutdown

--- a2/shutdown.go
+++ a2/shutdown.go
@@ -1,9 +1,12 @@
 package a2
 
 import (
+	"fmt"
+
 	"github.com/pevans/erc/asmrec"
 	"github.com/pevans/erc/disasm"
 	"github.com/pevans/erc/input"
+	"github.com/pevans/erc/internal/metrics"
 )
 
 // Shutdown will execute whatever is necessary to basically cease operation of
@@ -12,6 +15,12 @@ func (c *Computer) Shutdown() error {
 	disasm.Shutdown()
 	asmrec.Shutdown()
 	input.Shutdown()
+
+	fmt.Println("--- METRICS ---")
+	mets := metrics.Export()
+	for name, counter := range mets {
+		fmt.Printf("%v = %v\n", name, counter)
+	}
 
 	return nil
 }
