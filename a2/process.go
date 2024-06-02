957b569ea786e6bff817654bebc4fed1478b173c
Peter Evans <peter@modul8r.com>
2024-06-02 01:36:40 -0500

    Remove debugger trap (for now, ominously)

--- a2/process.go
+++ a2/process.go
@@ -11,13 +11,6 @@ func (c *Computer) Process() error {
 		return err
 	}
 
-	/*
-		fmt.Printf("ugh: %s\n", c.CPU.NextInstruction())
-		if c.CPU.NextInstruction() == "INC $C083,X" {
-			c.Debugger = true
-		}
-	*/
-
 	// Check if this is was a knock-knock on one of our bank switches
 	switch c.CPU.EffAddr {
 	case 0xC081, 0xC083, 0xC085, 0xC087, 0xC089, 0xC08B, 0xC08D, 0xC08F:
