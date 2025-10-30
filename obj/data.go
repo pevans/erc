4ed3ebc1955fe1e4e25f441a3184bf7089e08952
Peter Evans <peter@modul8r.com>
2025-10-30 16:19:54 -0500

    Actually remove store.go

--- obj/data.go
+++ obj/data.go
@@ -9,3 +9,16 @@ var systemROM []uint8
 
 //go:embed peripheral.rom
 var peripheralROM []uint8
+
+// SystemROM returns the embedded system ROM. This would be mapped over
+// $D000-$FFFF.
+func SystemROM() []uint8 {
+	return systemROM
+}
+
+// PeripheralROM returns the embedded peripheral ROM. This would be mapped
+// over $C000-$CFFF; the first page is zero bytes because that would normally
+// be used for soft switches.
+func PeripheralROM() []uint8 {
+	return peripheralROM
+}
