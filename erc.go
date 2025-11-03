c6401e54af149da627889a2cf202bada40e8b038
Peter Evans <peter@modul8r.com>
2025-11-03 15:12:40 -0600

    Remove unnecessary ConfigFile const

--- erc.go
+++ erc.go
@@ -4,9 +4,6 @@ import (
 	"github.com/pevans/erc/cmd"
 )
 
-// ConfigFile is the default (relative) location of our configuration file.
-const ConfigFile = `.erc/config.toml`
-
 func main() {
 	cmd.Execute()
 }
