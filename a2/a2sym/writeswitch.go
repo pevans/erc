291442d1c4e1694767f9dff8c8bd0b9e9fcfcf8f
Peter Evans <peter@modul8r.com>
2025-11-04 18:48:54 -0600

    Better descriptions

--- a2/a2sym/writeswitch.go
+++ a2/a2sym/writeswitch.go
@@ -8,8 +8,8 @@ var writeSwitchMap = map[int]Switch{
 	0xC010: {Mode: ModeW, Description: "clear strobe"},
 
 	// Display switches
-	0xC000: {Mode: ModeW, Name: "80STORE", Description: "off: cause PAGE2 on to select auxiliary ram"},
-	0xC001: {Mode: ModeW, Name: "80STORE", Description: "on: allow PAGE2 to switch main RAM areas"},
+	0xC000: {Mode: ModeW, Name: "80STORE", Description: "off: disallow PAGE2 from control of display memory"},
+	0xC001: {Mode: ModeW, Name: "80STORE", Description: "on: allow PAGE2 to control display memory"},
 	0xC00C: {Mode: ModeW, Name: "80COL", Description: "off: display 40 columns"},
 	0xC00D: {Mode: ModeW, Name: "80COL", Description: "on: display 80 columns"},
 	0xC00E: {Mode: ModeW, Name: "ALTCHAR", Description: "off: display text using primary character set"},
@@ -19,7 +19,7 @@ var writeSwitchMap = map[int]Switch{
 	0xC052: {Mode: ModeRW, Name: "MIXED", Description: "off: display only text or only graphics"},
 	0xC053: {Mode: ModeRW, Name: "MIXED", Description: "on: if TEXT off, display text and graphics"},
 	0xC054: {Mode: ModeRW, Name: "PAGE2", Description: "off: select display page 1"},
-	0xC055: {Mode: ModeRW, Name: "PAGE2", Description: "off: select display page 2 or, if 80STORE on, display page 1 in auxiliary memory"},
+	0xC055: {Mode: ModeRW, Name: "PAGE2", Description: "on: select display page 2 or, if 80STORE on, display page 1 in auxiliary memory"},
 	0xC056: {Mode: ModeRW, Name: "HIRES", Description: "off: if TEXT off, display low-resolution graphics"},
 	0xC057: {Mode: ModeRW, Name: "HIRES", Description: "on: if TEXT off, display high-resolution or, if DHIRES on, double-high-resolution graphics"},
 	0xC05E: {Mode: ModeRW, Name: "DHIRES", Description: "on: if IOUDIS on, turn on double-high res."},
