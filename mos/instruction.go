c9d0c9b27ff6ab81d050d787fb2ff4ee0c1c30c4
Peter Evans <peter@modul8r.com>
2025-11-05 17:45:59 -0600

    Use correct order for P and S registers

--- mos/instruction.go
+++ mos/instruction.go
@@ -171,7 +171,7 @@ func (c *CPU) Execute() error {
 func (c *CPU) Status() string {
 	return fmt.Sprintf(
 		"A:$%02X X:$%02X Y:$%02X S:$%02X P:$%02X (%s) PC:$%04X EA:$%04X EV:$%02X",
-		c.A, c.X, c.Y, c.P, c.S, formatStatus(c.P), c.PC, c.EffAddr, c.EffVal,
+		c.A, c.X, c.Y, c.S, c.P, formatStatus(c.P), c.PC, c.EffAddr, c.EffVal,
 	)
 }
 
