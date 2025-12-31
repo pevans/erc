0253b62ab860e5b5df1acb968c41c8e9d729e6b2
Peter Evans <peter@modul8r.com>
2025-12-31 13:04:37 -0600

    Better timeset docs

--- asm/timeset.go
+++ asm/timeset.go
@@ -10,21 +10,39 @@ import (
 	"golang.org/x/exp/maps"
 )
 
+// A TimeSetEntry records information about the execution of some instruction
+// that is recorded in a TimeSet.
 type TimeSetEntry struct {
-	// number of times an instruction was executed
-	Executed int
+	// executed is the number of times an instruction was executed
+	executed int
 
-	// How many total cycles were consumed by this instruction
-	CyclesConsumed int
+	// cyclesConsumed is the total number cycles that were consumed by this
+	// instruction
+	cyclesConsumed int
 }
 
+// A TimeSet is a map of the instructions that were executed during emulation.
+// Each instruction is paired with some metadata about the execution that can
+// be used to determine the rough time spent on execution for each
+// instruction.
 type TimeSet struct {
+	// calls is a map of instructions (the key) paired with TimeSetEntry
+	// values.
 	calls map[string]TimeSetEntry
-	mu    sync.Mutex
 
+	// mu is a mutex. We're likely to have a lot of writes and reads on the
+	// map that happen concurrently during operation.
+	mu sync.Mutex
+
+	// timePerCycle is our belief of the time that each cycle takes to be
+	// executed. This is something that is defined for us when the TimeSet is
+	// allocated.
 	timePerCycle time.Duration
 }
 
+// NewTimeSet returns a TimeSet oriented by the provided timePerCycle -- that
+// is, we will assume each cycle consumes timePerCycle, and will use that to
+// estimate the total cost of execution for each instruction.
 func NewTimeset(timePerCycle time.Duration) *TimeSet {
 	set := new(TimeSet)
 	set.timePerCycle = timePerCycle
@@ -34,6 +52,20 @@ func NewTimeset(timePerCycle time.Duration) *TimeSet {
 	return set
 }
 
+// formatted returns a formatted string representation of a TimeSetEntry, with
+// respect to a given TimeSet (i.e. which has a particular alignment of
+// timePerCycle).
+func (entry *TimeSetEntry) formatted(set *TimeSet) string {
+	return fmt.Sprintf(
+		"run %v cyc %v spent %v",
+		entry.executed,
+		entry.cyclesConsumed,
+		time.Duration(entry.cyclesConsumed)*set.timePerCycle,
+	)
+}
+
+// Record will add a call to the TimeSet and set the number of cycles consumed
+// by the provided arguments.
 func (set *TimeSet) Record(call string, cycles int) {
 	set.mu.Lock()
 	defer set.mu.Unlock()
@@ -41,12 +73,14 @@ func (set *TimeSet) Record(call string, cycles int) {
 	// If no entry existed previously, this will give us a blank record.
 	entry := set.calls[call]
 
-	entry.Executed++
-	entry.CyclesConsumed += cycles
+	entry.executed++
+	entry.cyclesConsumed += cycles
 
 	set.calls[call] = entry
 }
 
+// WriteToFile will write the contents of the timeset to some provided
+// filename. If that cannot be done, an error is returned.
 func (set *TimeSet) WriteToFile(file string) error {
 	set.mu.Lock()
 	defer set.mu.Unlock()
@@ -63,7 +97,7 @@ func (set *TimeSet) WriteToFile(file string) error {
 
 	for _, call := range calls {
 		entry := set.calls[call]
-		output := fmt.Sprintf("%-30v | %v\n", call, entry.Formatted(set))
+		output := fmt.Sprintf("%-30v | %v\n", call, entry.formatted(set))
 
 		if _, err := fp.WriteString(output); err != nil {
 			return err
@@ -71,13 +105,4 @@ func (set *TimeSet) WriteToFile(file string) error {
 	}
 
 	return nil
-}
-
-func (entry *TimeSetEntry) Formatted(set *TimeSet) string {
-	return fmt.Sprintf(
-		"run %v cyc %v spent %v",
-		entry.Executed,
-		entry.CyclesConsumed,
-		time.Duration(entry.CyclesConsumed)*set.timePerCycle,
-	)
 }
