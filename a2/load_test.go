6d7848bcdfb447aec175f72464ba9ef422239cb3
Peter Evans <peter@modul8r.com>
2025-12-31 11:18:20 -0600

    Don't save the test disk

--- a2/load_test.go
+++ a2/load_test.go
@@ -8,5 +8,9 @@ func (s *a2Suite) TestComputerLoad() {
 	dat, _ := os.Open("../data/logical.disk")
 	s.NoError(s.comp.Load(dat, "something.dsk"))
 
+	// If we run Load again without calling RemoveDisk, it'll try and save
+	// "something.dsk"
+	s.comp.SelectedDrive.RemoveDisk()
+
 	s.Error(s.comp.Load(nil, "bad"))
 }
