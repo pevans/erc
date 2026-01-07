318767f888d5463ff01d3049a492f8d9de0eccf7
Peter Evans <peter@modul8r.com>
2026-01-07 11:09:07 -0600

    Add locking to handle racing in the process loop
    In particular, you would see some fullspeed emulation immediately
    following speed changes and pauses

--- clock/emulator.go
+++ clock/emulator.go
@@ -3,6 +3,7 @@ package clock
 import (
 	"fmt"
 	"log/slog"
+	"sync"
 	"time"
 
 	"github.com/pevans/erc/a2/a2state"
@@ -34,6 +35,15 @@ type Emulator struct {
 	// but is instead moving as fast as it can.
 	fullSpeed bool
 
+	// timingMu protects timing-related fields (hertz, timePerCycle,
+	// resumeTime, totalCycles) from concurrent access between the process
+	// loop and speed change operations.
+	timingMu sync.Mutex
+
+	// timingEpoch is incremented whenever timing is reset. The process loop
+	// uses this to detect when it should break out and recalculate wantedCycles.
+	timingEpoch uint64
+
 	// debuggerEntryFunc is a function we'll run whenever we have gone into
 	// debugging mode.
 	debuggerEntryFunc func()
@@ -56,19 +66,26 @@ func NewEmulator(hz int64) *Emulator {
 // ChangeHertz resets the clock emulation's hertz value and expected time
 // per cycle.
 func (e *Emulator) ChangeHertz(hz int64) {
+	e.timingMu.Lock()
+	defer e.timingMu.Unlock()
+
 	e.hertz = hz
 	e.timePerCycle = (1 * time.Second) / time.Duration(hz)
 
 	// Reset timing so we don't think we're behind or ahead on cycles
 	e.resumeTime = time.Now()
 	e.totalCycles = 0
+	e.timingEpoch++
 }
 
 // ProcessLoop runs the execution process for the provided computer. It does
 // not exit unless there was some problem.
 func (e *Emulator) ProcessLoop(comp emu.Computer) {
+	e.timingMu.Lock()
 	e.startTime = time.Now()
 	e.resumeTime = e.startTime
+	e.timingMu.Unlock()
+
 	state := comp.StateMap()
 
 	wasPaused := false
@@ -81,8 +98,7 @@ func (e *Emulator) ProcessLoop(comp emu.Computer) {
 
 			// Reset ResumeTime so that we don't think we're far behind on
 			// cycle time just because we sat in the debugger for a while
-			e.resumeTime = time.Now()
-			e.totalCycles = 0
+			e.resetTiming()
 
 			continue
 		}
@@ -97,18 +113,22 @@ func (e *Emulator) ProcessLoop(comp emu.Computer) {
 		// Paused status was unset.
 		if wasPaused {
 			wasPaused = false
-			e.resumeTime = time.Now()
-			e.totalCycles = 0
+			e.resetTiming()
 		}
 
+		e.timingMu.Lock()
 		elapsed := time.Since(e.resumeTime)
 		wantedCycles := int64(elapsed / e.timePerCycle)
+		totalCycles := e.totalCycles
+		fullSpeed := e.fullSpeed
+		epoch := e.timingEpoch
+		e.timingMu.Unlock()
 
 		// There are times when we ignore cycle timing and want to emulate as
 		// fast as possible -- should that happen, it's up to the instructions
 		// we're processing to unset FullSpeed.
-		for e.totalCycles < wantedCycles || e.fullSpeed {
-			wereFullSpeed := e.fullSpeed
+		for totalCycles < wantedCycles || fullSpeed {
+			wereFullSpeed := fullSpeed
 
 			cycles, err := comp.Process()
 			if err != nil {
@@ -116,17 +136,37 @@ func (e *Emulator) ProcessLoop(comp emu.Computer) {
 				return
 			}
 
+			e.timingMu.Lock()
 			e.totalCycles += int64(cycles)
+			totalCycles = e.totalCycles
+			fullSpeed = e.fullSpeed
+			currentEpoch := e.timingEpoch
+			e.timingMu.Unlock()
+
+			// If timing was reset (speed change, pause, etc), break out and
+			// recalculate wantedCycles with the new timing values
+			if currentEpoch != epoch {
+				break
+			}
 
 			// When transitioning out of full speed, reset the timing so we
 			// don't think we're behind and need to catch up
-			if wereFullSpeed && !e.fullSpeed {
-				e.resumeTime = time.Now()
-				e.totalCycles = 0
+			if wereFullSpeed && !fullSpeed {
+				e.resetTiming()
 				break
 			}
 		}
 	}
+}
+
+// resetTiming resets the timing state after pause, debugger, or full-speed
+// transitions.
+func (e *Emulator) resetTiming() {
+	e.timingMu.Lock()
+	defer e.timingMu.Unlock()
+	e.resumeTime = time.Now()
+	e.totalCycles = 0
+	e.timingEpoch++
 }
 
 // SetBreakpointCheck will use the provided function, f, to test if a
@@ -145,6 +185,8 @@ func (e *Emulator) SetDebuggerEntry(f func()) {
 // provided status, telling us either to go at full speed (true) or to emulate
 // software at the normal hertz.
 func (e *Emulator) SetFullSpeed(status bool) {
+	e.timingMu.Lock()
+	defer e.timingMu.Unlock()
 	e.fullSpeed = status
 }
 
