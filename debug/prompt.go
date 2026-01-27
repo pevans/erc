bbd11ded9a07a1aa03a06a2cb5c0de45a6aeaed1
Peter Evans <peter@modul8r.com>
2026-01-26 22:23:39 -0600

    Remove unused function, read

--- debug/prompt.go
+++ debug/prompt.go
@@ -1,9 +1,6 @@
 package debug
 
 import (
-	"bufio"
-	"os"
-
 	"github.com/peterh/liner"
 	"github.com/pevans/erc/a2"
 )
@@ -17,17 +14,4 @@ func Prompt(comp *a2.Computer, line *liner.State) {
 
 	line.AppendHistory(cmd)
 	execute(comp, cmd)
-}
-
-func read() (string, error) {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// Since we're just scanning one line, we don't really care what the
-	// result of Scan is
-	_ = scanner.Scan()
-
-	line := scanner.Text()
-	err := scanner.Err()
-
-	return line, err
 }
